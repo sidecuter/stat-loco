200b2a95288f9127fb744fd66b2533440832cf31
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-04-27 18:49:53 +0300

    Reformat with cargo fmt

--- src/views/pagination.rs
+++ src/views/pagination.rs
@@ -37,17 +37,17 @@ impl PaginationResponse {
         } else {
             pagination_query.page - 1
         };
-    
+
         let query = entity.paginate(db, pagination_query.page_size);
         let total_pages_and_items = query.num_items_and_pages().await?;
         let page: Vec<M> = query.fetch_page(page).await?;
-    
+
         let paginated_response = PageResponse {
             page,
             total_pages: total_pages_and_items.number_of_pages,
             total_items: total_pages_and_items.number_of_items,
         };
-    
+
         Ok(paginated_response)
     }
 }
